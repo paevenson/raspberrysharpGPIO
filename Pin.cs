f5a5fafb5b7a7f93a0f9148b8598bfc3a9a9a1d6
paevenson <paevenson@gmail.com>
2016-10-09 22:03:00 -0400

    Add files via upload

---
+++ Pin.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
+
+namespace RaspberrySharpGPIOLib
+{
+    public class Pin
+    {
+        public static int QUERY_IN_INTERVAL = 10; //10 ms
+
+        public const string GPIO_PATH = "/sys/class/gpio/";
+
+        public enum DIRECTION { IN, OUT, NONE };
+
+        //Read In Thread
+        private Thread m_QueryInThread;
+        private bool m_StopQueryThread = false;
+
+        bool m_IsExported = false;
+
+        int m_PinId;
+
+        DIRECTION m_PinDiection = DIRECTION.NONE;
+
+        int m_Value = 0;
+
+        public event Action<int, bool> ExportChanged; //<PinId, Export>
+        public event Action<int, DIRECTION> DirectionChanged; //<PinId, Direction>
+        public event Action<int, int> ValueChanged; //<PinId, Value>
+
+        public bool IsExported
+        {
+            get { return m_IsExported; }
+            private set
+            {
+                //retru if we arent changing
+                if ((value == true && m_IsExported == true) || (value == false && m_IsExported == false))
+                    return;
+
+                ChangeExportState(value);
+            }
+        }
+
+        public DIRECTION Direction
+        {
+            get { return m_PinDiection; }
+            set
+            {
+                //export it
+                if (IsExported == false)
+                    IsExported = true;
+
+                if (m_PinDiection != value)
+                    ChangeDirectionState(value);
+            }
+        }
+
+        public int Value
+        {
+            get { return m_Value; }
+            set
+            {
+                if (IsExported == false)
+                    IsExported = true;
+
+                if (m_Value != value)
+                    ChangePinValue(value);
+            }
+        }
+
+        public Pin(int id)
+        {
+            m_PinId = id;
+
+            if(ReadExport())
+                CleanUpPin(true);
+
+            IsExported = true;
+
+            m_Value = ReadPinValue();
+        }
+
+        public void CleanUpPin(bool force = false)
+        {
+            IsExported = false;
+
+            if(m_QueryInThread != null && m_QueryInThread.IsAlive)
+                m_StopQueryThread = true;
+
+            if (force)
+                ChangeExportState(false);
+        }
+
+        private int ReadPinValue()
+        {
+            return ReadIO(GPIO_PATH + "gpio" + m_PinId.ToString() + "/value");
+        }
+
+        private bool ReadExport()
+        {
+            return Directory.Exists(GPIO_PATH + "gpio" + m_PinId.ToString());
+        }
+
+        private DIRECTION ReadDirection()
+        {
+            return (DIRECTION)ReadIO(GPIO_PATH + "gpio" + m_PinId.ToString() + "/direction");
+        }
+
+        private int ReadIO(string path)
+        {
+            string filename = GPIO_PATH + "gpio" + m_PinId.ToString() + "/value";
+            if (File.Exists(path))
+            {
+                string readValue = File.ReadAllText(path);
+                if (readValue != null && readValue.Length > 0 && readValue[0] == '1')
+                    return 1;
+            }
+
+            return 0;
+        }
+
+        private void ChangeExportState(bool export)
+        {
+            File.WriteAllText(GPIO_PATH + (export ? "export":"unexport"), m_PinId.ToString());
+            m_IsExported = export;
+
+            if (ExportChanged != null)
+                ExportChanged(m_PinId, m_IsExported);
+        }
+
+        //
+        private void ChangeDirectionState(DIRECTION direction)
+        {
+            File.WriteAllText(GPIO_PATH + "gpio" + m_PinId.ToString() + "/direction", direction.ToString().ToLowerInvariant());
+
+            //start up the thread
+            if(direction == DIRECTION.IN)
+            {
+                m_QueryInThread = new Thread(ReadPinThread);
+                m_QueryInThread.Start();
+            }
+            else if(m_PinDiection == DIRECTION.IN && direction != DIRECTION.IN)
+            {
+                m_StopQueryThread = true;   //Stop our thread
+                while (m_StopQueryThread == true) Thread.Sleep(2); //wait for the thread to stop...
+            }
+
+            m_PinDiection = direction;
+
+            if (DirectionChanged != null)
+                DirectionChanged(m_PinId, m_PinDiection);
+        }
+
+        //0 == low, 1 == high
+        private void ChangePinValue(int pinValue)
+        {
+            if(Direction == DIRECTION.OUT)
+                File.WriteAllText(GPIO_PATH + "gpio" + m_PinId.ToString() + "/value", pinValue.ToString());
+            m_Value = pinValue;
+
+            if (ValueChanged != null)
+                ValueChanged(m_PinId, pinValue);
+        }
+
+        //loops based in the query_in_interval
+        private void ReadPinThread()
+        {
+            while(m_StopQueryThread == false)
+            {
+                try
+                {
+                    this.Value = ReadPinValue();
+                }
+                catch  {  }
+
+                Thread.Sleep(QUERY_IN_INTERVAL);
+            }
+
+            m_StopQueryThread = false;
+        }
+    }
+}
---
+++ RaspberrySharpGPIO.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaspberrySharpGPIOLib
+{
+    //map the pin# to its gpio#
+    public enum PIN { pin3 = GPIO.gpio2, pin5 = GPIO.gpio3, pin7 = GPIO.gpio4, pin8 = GPIO.gpio14, pin10 = GPIO.gpio15, pin11 = GPIO.gpio17, pin12 = GPIO.gpio18, pin13 = 27, pin15 = GPIO.gpio22, pin16 = GPIO.gpio23, pin18 = GPIO.gpio24, pin19 = GPIO.gpio10, pin21 = GPIO.gpio9, pin22 = GPIO.gpio25, pin23 = GPIO.gpio11, pin24 = GPIO.gpio8, pin26 = GPIO.gpio7, pin29 = GPIO.gpio5, pin31 = GPIO.gpio6, pin32 = GPIO.gpio12, pin33 = GPIO.gpio13, pin35 = GPIO.gpio19, pin36 = GPIO.gpio16, pin37 = GPIO.gpio26, pin38 = GPIO.gpio20, pin40 = GPIO.gpio21 };
+
+    //map the gpio to its # (for easy file access)
+    public enum GPIO { gpio2 = 2, gpio3 = 3, gpio4 = 4, gpio5 = 5, gpio6 = 6, gpio7 = 7, gpio8 = 8, gpio9 = 9, gpio10 = 10, gpio11 = 11, gpio12 = 12, gpio13 = 13, gpio14 = 14, gpio15 = 15, gpio16 = 16, gpio17 = 17, gpio18 = 18, gpio19 = 19, gpio20 = 20, gpio21 = 21, gpio22 = 22, gpio23 = 23, gpio24 = 24, gpio25 = 25, gpio26 = 26 };
+
+    public class RaspberrySharpGPIO
+    {
+        #region Singleton
+        private static RaspberrySharpGPIO m_Instance = null;
+        public static RaspberrySharpGPIO Instance
+        {
+            get
+            {
+                if (m_Instance == null)
+                {
+                    m_Instance = new RaspberrySharpGPIO();
+                }
+
+                return m_Instance;
+            }
+        }
+
+        #endregion 
+
+        private Dictionary<int, Pin> m_PinTable = new Dictionary<int, Pin>();
+
+        #region overload []
+        public Pin this[GPIO i]
+        {
+            get { return this[(int)i]; }
+        }
+
+        public Pin this[PIN i]
+        {
+            get { return this[(int)i]; }
+        }
+
+        private Pin this[int i]
+        {
+            get
+            {
+                if(!m_PinTable.ContainsKey(i))
+                    m_PinTable.Add(i, new Pin(i));
+
+                return m_PinTable[i];
+            }
+        }
+        #endregion
+
+        #region SetPinDirections
+        public void SetPinDirections(Pin.DIRECTION direction, params GPIO[] gpios)
+        {
+            foreach (GPIO gpio in gpios)
+            {
+                this[gpio].Direction = direction;
+            }
+        }
+
+        public void SetPinDirections(Pin.DIRECTION direction, params PIN[] pins)
+        {
+            foreach (PIN pin in pins)
+            {
+                this[pin].Direction = direction;
+            }
+        }
+
+        public void SetPinDirections(Pin.DIRECTION direction, params Pin[] pins)
+        {
+            foreach (Pin pin in pins)
+            {
+                pin.Direction = direction;
+            }
+        }
+        #endregion
+
+        public void CleanUpAllPins()
+        {
+            foreach (int key in m_PinTable.Keys)
+                m_PinTable[key].CleanUpPin();
+        }
+    }
+}
